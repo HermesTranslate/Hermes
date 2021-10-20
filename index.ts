b034ebc51b3df134457552c4a73abaec53926b51
Justin Looper <32334634+cyberlooper@users.noreply.github.com>
2021-10-20 09:49:49 +0100

    New reworked and using WOKCommands library

---
+++ index.ts
@@ -0,0 +1,25 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+import WOKCommands from 'wokcommands'
+import path from 'path'
+dotenv.config();
+
+const client = new DiscordJS.Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+});
+
+client.on("ready", () => {
+  console.log("The bot is Ready!")
+
+  new WOKCommands(client, {
+    commandsDir: path.join(__dirname, 'commands'),
+    typeScript: true,
+    testServers: '897594588590645278'
+  })
+})
+
+client.login(process.env.TOKEN);
